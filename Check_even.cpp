39f1dc7236f1156c47582edd52a34a3657ab2bf4
YashSinghal-cloud <singhalyash744@gmail.com>
2024-12-27 00:03:40 +0530

    Check_even.cpp

--- Check_even.cpp
+++ Check_even.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int even_odd(int a){
-    if (a%2==0){
-    return 1;
+int even_odd(int num)
+{
+    if (num % 2 == 0)
+    {
+        return 1;
     }
-    else{
-    return 0;
+    else
+    {
+        return 0;
     }
 }
 
